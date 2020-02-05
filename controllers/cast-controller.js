b3c09bba060f0b8753297cb4f60a68412d42d851
Krizel Minnema <krizelminnema@gmail.com>
2020-02-04 21:28:32 -0800

    updating eslint

--- controllers/cast-controller.js
+++ controllers/cast-controller.js
@@ -3,21 +3,21 @@ var router = express.Router();
 var cast = require("../models/castr_model");
 
 //CHANGE TO GET DATA FROM THE DB TO POST ONTO HOME PAGE
-router.get('/', function(req, res){
-    res.redirect('/landing') //Redirect to home page
+router.get("/", function(req, res){
+    res.redirect("/landing") //Redirect to home page
 });
 
-router.get('/landing', function(req,res){
+router.get("/landing", function(req,res){
     cast.all(function(data){ //Display all burgers on home page
         var postsObj = {posts: data};
         console.log(postsObj);
-        res.render('index', postsObj);
+        res.render("index", postsObj);
     });
 });
 
 //POSTS PAGE FOR ADDING, EDITING, DELETING POSTS ---
-router.get('/posts', function(req,res){
-    res.render('newpost', {layout: 'newpost.handlebars'});
+router.get("/posts", function(req,res){
+    res.render("newpost", {layout: "newpost.handlebars"});
 });
 
 
@@ -41,8 +41,8 @@ router.post("/posts/add", function(req, res){
 //////////////////////////// WORKING ////////////////////////////
 
 //POSTS PAGE FOR ADDING, EDITING, DELETING POSTS ---
-router.get('/posts', function(req,res){
-    res.render('newpost', {layout: 'newpost.handlebars'});
+router.get("/posts", function(req,res){
+    res.render("newpost", {layout: "newpost.handlebars"});
 });
 
 
@@ -59,7 +59,7 @@ router.post("/comments/:id", function(req, res){
     });
 });
 
-//CHANGE SO THIS CAN EDIT A USER'S POSTS
+//CHANGE SO THIS CAN EDIT A USER"S POSTS
 router.put("/posts/update/:id", function(req, res){
 	var condition = "id=" + req.params.id;
 	console.log("condition", condition);
@@ -71,11 +71,11 @@ router.put("/posts/update/:id", function(req, res){
         post_body: req.body.post_body,
         post_image: req.body.post_image
 	}, condition, function(data){
-		res.render('index');
+		res.render("index");
 	});
 });
 
-//CHANGE SO THIS CAN DELETE A USER'S POSTS
+//CHANGE SO THIS CAN DELETE A USER"S POSTS
 router.delete("/posts/delete/:id", function(req, res) {
 
     var condition = "id = " + req.params.id;
