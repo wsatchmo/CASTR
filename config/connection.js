953ca58d960135a1389d5663e6a488ff6a787774
wsatchmo <wsatcher@outlook.com>
2020-04-03 12:43:42 -0700

    Heroku build testing

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,15 @@ const mysql = require("mysql"); //mysql dependency
 const dotenv = require("dotenv").config(); //dotenv dependency
 let connection;
 
-if (process.env.JAWSDB_MARIA_URL){ //Jaws db connection
-    connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
-} else {
+if (process.env.JAWSDB_URL){ //Jaws db connection
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { //Otherwise local connection
     connection = mysql.createConnection({
-        host: process.env.DB_HOST, //Create .env file & fill_
-        user: process.env.DB_USER, //in your connection info_
+        host: process.env.DB_HOST_LOCAL, //Create .env file & fill_
+        user: process.env.DB_USER_LOCAL, //in your connection info_
         port: 3306,                //equal to these variables
-        password: process.env.DB_PASS, 
-        database:  process.env.DATABASE
+        password: process.env.DB_PASS_LOCAL, 
+        database:  process.env.DATABASE_LOCAL
     });
 };
 
